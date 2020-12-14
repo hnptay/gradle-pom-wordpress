f1ebda5d9433bc11a7e8b812c3d1f4658e019530
YAT <hnptay@outlook.com>
2020-12-14 14:50:18 +0700

    ALL

---
+++ src/main/java/browserFactory/DriverManager.java
@@ -0,0 +1,28 @@
+package browserFactory;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class DriverManager {
+    protected WebDriver driver;
+
+    protected abstract void createDriver();
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver(String url) {
+        if (driver == null) createDriver();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public WebDriver getDriverForScreenshot(){
+        return driver;
+    }
+}
---
+++ src/main/java/pageObject_jQuery/PageGeneratorManager.java
@@ -0,0 +1,9 @@
+package pageObject_jQuery;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageGeneratorManager {
+    public static DataTablePageObject getDataTablePage(WebDriver driver){
+        return new DataTablePageObject(driver);
+    }
+}
